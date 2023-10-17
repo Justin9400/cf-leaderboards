8f4c3c708a8dbdcaa645c20bc81aae5f2544d96e
Brandon Camerer <50874245+brandoncamerer@users.noreply.github.com>
2023-10-17 15:53:49 -0700

    lintsucks

--- src/components/leaderboard/leaderboard.tsx
+++ src/components/leaderboard/leaderboard.tsx
@@ -5,7 +5,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { GameInfoMap } from '../../maps/GameInfoMap';
 import { Game } from '../../models/models';
 
