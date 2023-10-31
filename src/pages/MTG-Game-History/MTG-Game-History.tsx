c85ae662f62ebe3bd43ef3ec6dd7b07eddb9e736
Brandon Camerer <50874245+brandoncamerer@users.noreply.github.com>
2023-10-31 11:02:54 -0700

    lol

--- src/pages/MTG-Game-History/MTG-Game-History.tsx
+++ src/pages/MTG-Game-History/MTG-Game-History.tsx
@@ -33,7 +33,8 @@ function MTGGameHistory() {
           transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
           borderRadius: '4px',
           boxShadow:
-            '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)'
+            '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)',
+          maxHeight: '60%'
 
           // Add more styles as needed
         }}
