fc1c196e71f77139c7c3a0708415730a666ef838
Brandon Camerer <50874245+brandoncamerer@users.noreply.github.com>
2023-11-01 08:00:11 -0700

    fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -25,7 +25,7 @@ function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="/"
+            href="/home"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
