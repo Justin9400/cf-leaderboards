5ff72f663b785377d7c13c9ca31e4ca999c420dd
bryan <76141379+bberardi@users.noreply.github.com>
2023-11-06 01:07:33 +0000

    gif

--- src/pages/Foosball-Leaderboard/foosball-leaderboard.tsx
+++ src/pages/Foosball-Leaderboard/foosball-leaderboard.tsx
@@ -1,11 +1,12 @@
 import { Stack } from '@mui/system'
+import working from '../../img/working.gif'
 
 function FoosballLeaderBoard() {
   return (
     <>
-      <Stack sx={{ display: 'flex', height: '100vh', margin: '0 auto' }}>
+      <Stack sx={{ display: 'flex', height: '100vh', margin: '0 auto', alignItems: 'center' }}>
         <h1 style={{ margin: '5% auto', fontFamily: 'Arial, Helvetica, sans-serif' }}>Foosball Leaderboard</h1>
-        <img src="/src/img/working.gif" alt="sucks to not see the gif (jif) huh" />
+        <img src={working} alt="sucks to not see the gif (jif) huh" style={{ width: '60%', height: 'auto' }} />
       </Stack>
     </>
   )
